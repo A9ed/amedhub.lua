dd832e06d0abae1005b7d5e0e789f18df4646066
A9ed <157604856+A9ed@users.noreply.github.com>
2025-06-27 17:19:43 +0300

    Update amedhub.lua
    local ScreenGui = Instance.new("ScreenGui")
    local Frame = Instance.new("Frame")
    ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
    Frame.Parent = ScreenGui
    Frame.BackgroundColor3 = Color3.fromRGB(0, 150, 255)
    Frame.Size = UDim2.new(0, 300, 0, 200)
    Frame.Position = UDim2.new(0.3, 0, 0.3, 0)

--- amedhub.lua
+++ amedhub.lua
@@ -1,1 +1,7 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AmedHubDev/MaruKeyless-Amed/main/amedhub.lua"))()
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(0, 150, 255)
+Frame.Size = UDim2.new(0, 300, 0, 200)
+Frame.Position = UDim2.new(0.3, 0, 0.3, 0)
